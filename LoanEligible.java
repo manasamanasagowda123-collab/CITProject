f3fd3c0ef26744a3c44b9d7321a917c473078e33
manasamanasagowda123-collab <manasamanasagowda123@gmail.com>
2026-03-26 06:15:29 +0530

    initial commit for java project

---
+++ LoanEligible.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LoanEligible {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age;
+        float salary;
+        System.out.println("Enter Age:" ) ;
+        age= sc.nextInt();
+        System.out.println("Enter Salary:" ) ;
+        salary= sc.nextInt();
+        if(age>=21 && salary>=21000)
+          System.out.println("Eligible to take loan");
+        else
+            System.out.println("not Eligible");
+        sc.close();
+}
+}
---
+++ Units.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Units {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of units: ");
+        int units;
+        double bill;
+        System.out.println("Enter the units from user");
+        units = sc.nextInt();
+        if (units <= 150)
+            bill = units * 1.5;
+        else
+            bill = units * 3;
+        System.out.println("Final electric bill=" + bill);
+        sc.close();
+    }
+
+}
